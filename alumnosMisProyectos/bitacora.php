5347fde3f6ae077ca03662875f3eacd6e130a5db
vitorioni <vortuzar@alumnos.uai.cl>
2016-06-01 01:44:47 -0300

    bitacora creada por alumnos

--- alumnosMisProyectos/bitacora.php
+++ alumnosMisProyectos/bitacora.php
@@ -28,6 +28,7 @@ $mysqli = new mysqli("localhost", "root", "", "moodle", 3306);
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 
 
+
 // Moodle pages require a context, that can be system, course or module (activity or resource)
 $context = context_system::instance();
 $PAGE->set_context($context);
