462638d1fa2db0be6ba753e28b77d990e3e56954
vitorioni <vortuzar@alumnos.uai.cl>
2016-06-01 01:47:38 -0300

    pestaña VerProyectos, vista alumnos(en local)

--- alumnosVerProyectos/version.php
+++ alumnosVerProyectos/version.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+
 /**
  * Defines the version of emarking
  * This code fragment is called by moodle_needs_upgrading() and
